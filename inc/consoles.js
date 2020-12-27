4a9ca0cda005e55ac16d45db41a5a03b6470fa70
Erick <yaeriks@yandex.ru>
2020-12-27 16:17:18 +0200

    Add files via upload

---
+++ inc/consoles.js
@@ -0,0 +1,26 @@
+ $(function(){
+  $(document).keypress(function(e) {
+    cwrite(e.which,'Keypress event');
+    if(e.which==120) custom_dialog_toggle('Keypress x','You opened this window by pressing x');
+  });
+});
+function custom_dialog_toggle(title,text,buttons) {
+  if(typeof title !=='undefined') $('#dlg-header').html(title);
+  if(typeof text !=='undefined') $('#dlg-content').html(text);
+  cwrite('Current state: '+$('#dialog_state').prop("checked"),'custom_dialog_toggle');
+  $('#dialog_state').prop("checked", !$('#dialog_state').prop("checked"));
+}
+// Console logging function for debugging
+// cwrite(str, title)
+//      str:              string to be appended to console
+//      title (optional): title of the string
+// (c)  Tuomas Hatakka 2015
+//      http://tuomashatakka.fi
+function cwrite(str,title) {
+  var ce = $('#console');
+  var sg = "<p>";
+  if(typeof title !=='undefined') sg = sg+"<em>"+title+"</em> ";
+  sg = sg+str+"</p>";
+  ce.prepend(sg);
+  //if(ce.children("p").length>6) ce.children("p").first().remove();
+}
