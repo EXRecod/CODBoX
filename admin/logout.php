440b854e8eb8a6714eac46b413d704a386717bb8
Erick <yaeriks@yandex.ru>
2021-06-16 00:04:13 +0300

    Add files via upload

--- admin/logout.php
+++ admin/logout.php
@@ -1,21 +1,19 @@
 <?php 
-if (empty($cpath)) { 
-  $cpath = dirname(__FILE__);
-  $xcpath = $cpath;
-}
-include($cpath ."/inc/data.php");
-include($xcpath ."/core/functions.php");
-include($xcpath ."/core/class.simpleSQLinjectionDetect.php");
-$templ = 1;
-  echo "Logout Successfully ";
-  session_destroy();
-  setcookie("codbx_u", "", time() - 36000);
-  setcookie("codbx_p", "", time() - 36000);
-  setcookie("user_online_key", "", time() - 36000);
-  setcookie("user_online_login", "", time() - 36000);
-  $_SESSION['codbxuser'] = '';
-  $_SESSION['codbxpass'] = '';
-  $_SESSION['codbxpasssteam'] = '';
-  $baseurlz = basename(__FILE__); 
-if((strpos($_SERVER['REQUEST_URI'], $domain)) !== false) header($_SERVER['REQUEST_URI']); else header("Location:$domain/index.php");
+session_start();
+header("Expires: Mon, 26 Jul 1995 05:00:00 GMT"); # Date In The Past
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); # Always Modified
+header("Cache-Control: no-store, no-cache, must-revalidate"); # HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache"); # HTTP/1.0
+ 
+session_destroy();
+setcookie("codbx_u", ".", time() - 36000000);
+setcookie("codbx_p", ".", time() - 36000000);
+setcookie("user_online_key", ".", time() - 36000000);
+setcookie("user_online_login", ".", time() - 36000000);
+ 
+$tag = str_replace("admin/logout.php", "index.php", $_SERVER['REQUEST_URI']); 
+?>
+<script>window.location.replace(<?php echo '"',$tag,'"';?>);</script>
+<?php
 ?>
